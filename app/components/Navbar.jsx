436bfd65a8c1f4d4e4001ffdb33e27402cf0b73c
willclem100 <will.clem@biteninja.com>
2023-09-21 23:20:26 -0500

    Start lesson 27 - has a bug

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,24 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
 import Logo from './dojo-logo.png'
 
-export default function Navbar() {
+export default function Navbar({ user }) {
   return (
     <nav>
-        <Image
-            src={Logo}
-            width={70}
-            alt="Dojo Helpdesk logo"
-            quality={100}
-            placeholder='blur'
-        />
-        <h1>Dojo Helpdesk</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk logo"
+        width={70}
+        placeholder="blur"
+        quality={100}
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
+      {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   )
 }
